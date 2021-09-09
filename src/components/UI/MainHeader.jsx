629c10438975485ed3e7c57e18099308b00189b3
Sonya-M <sonja.musicki@gmail.com>
2021-09-09 21:28:09 +0200

    navbar now shows candidate and logout btn only when a user is logged in

--- src/components/UI/MainHeader.jsx
+++ src/components/UI/MainHeader.jsx
@@ -4,18 +4,11 @@ import Header from "./Header";
 import LogoutBtn from "./LogoutBtn";
 
 export default function MainHeader(props) {
-  const homeLink = (<Link to="/">Interview Reports</Link>)
-  const candidatesLink = (<Link to="/">Candidates</Link>)
-  return (
-    <Header
-      title={homeLink}
-      menuItems={[candidatesLink, < LogoutBtn onLogout={props.onLogout}/>]}
-    />
-  )
+  const homeLink = <Link to="/">Interview Reports</Link>;
+  let menu = [];
+  const candidatesLink = <Link to="/">Candidates</Link>;
+  if (props.loggedIn) {
+    menu = [candidatesLink, <LogoutBtn onLogout={props.onLogout} />];
+  }
+  return <Header title={homeLink} menuItems={menu} />;
 }
-
-// function LogoutBtn(props) {
-//   return (
-//     <BoxArrowRight size="1rem" className={ LogoutBtn }onClick={ props.onLogout}/>
-//   )
-// }
