ae818bdc43f3da84507e40bdc572ced97ce0cce5
NikolaJankovic-dev <dzoni6usb@gmail.com>
2021-09-09 22:37:50 +0100

    Working table with dummy data

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,18 +1,17 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { useParams, useLocation, Link } from "react-router-dom";
-import { candidates as cant }   from "../data/candidates";
+import { candidates as cant } from "../data/candidates";
 import { reports } from "../data/reports";
 import { companies } from "../data/companies";
 import { getCandidates, getCompanies, getReports } from "../services/services";
-import LoginRedirect from "../components/LoginRedirect"
+import LoginRedirect from "../components/LoginRedirect";
 
-
-import style from './Report.module.css'
-import { Table } from './Table';
+import style from "./Report.module.css";
+import { Table } from "./Table";
 
 export default function Report(props) {
-  const {loggedIn} = props;
-  let location = useLocation()
+  const { loggedIn } = props;
+  let location = useLocation();
   let { id } = useParams(); // candidate id
   // just testing data fetching, state will probably not be here
   const [candidates, setCandidates] = useState([]);
@@ -39,14 +38,13 @@ export default function Report(props) {
   //     setCompanies(data);
   //   });
   // }, []);
-  
-  const selectedCandidateReport = reports
-  const selectedCandidate = cant.find(c => c.id === +id)
-  const bDay = new Date (selectedCandidate.birthday)
-  const formatedBirthday = `${bDay.getDate()}.${bDay.getMonth()+1}.${bDay.getFullYear()}`;
- 
-  
 
+  const selectedCandidateReport = reports;
+  const selectedCandidate = cant.find((c) => c.id === +id);
+  const bDay = new Date(selectedCandidate.birthday);
+  const formatedBirthday = `${bDay.getDate()}.${
+    bDay.getMonth() + 1
+  }.${bDay.getFullYear()}`;
 
   if (!loggedIn) {
     return <LoginRedirect redirectPath={location} />;
@@ -54,46 +52,40 @@ export default function Report(props) {
 
   return (
     <Fragment>
-      <h1>{`CANDIDATE REPORT (ID: ${id})`}</h1>
-    <div className="container">
-      <div className="row">
-        <div className="col">
-        <img src="https://picsum.photos/id/237/300/300"/>
-        </div>
-        <div className="col">
-          <div className={style.cardInfo}>
-            <p className={style.candidateInfo}>Name:</p>
-            <span className={style.candidateData}>
-            {selectedCandidate.name}
-            </span>
+      <div className={`container ${style.data}`}>
+        <div className="row">
+          <div className="col">
+            <img src="https://picsum.photos/id/237/300/300" />
           </div>
-          <div className={style.cardInfo}>
-          <p className={style.candidateInfo}>Email:</p>
-          <span className={style.candidateData}>
-            {selectedCandidate.email}
-            </span>
-          </div>
-
-        </div>
-        <div className="col">
-        <div className={style.cardInfo}>
-        <p className={style.candidateInfo}>Birthday:</p>
-        <span className={style.candidateData}>
-            {formatedBirthday}
-            </span>
-          </div>
-          <div className={style.cardInfo}> 
-          <p className={style.candidateInfo}>Education:</p>
-          <div className={style.candidateData}>
-            {selectedCandidate.education}
+          <div className="col">
+            <div className={style.cardInfo}>
+              <p className={style.candidateInfo}>Name:</p>
+              <span className={style.candidateData}>
+                {selectedCandidate.name}
+              </span>
+            </div>
+            <div className={style.cardInfo}>
+              <p className={style.candidateInfo}>Email:</p>
+              <span className={style.candidateData}>
+                {selectedCandidate.email}
+              </span>
             </div>
           </div>
-
+          <div className="col">
+            <div className={style.cardInfo}>
+              <p className={style.candidateInfo}>Birthday:</p>
+              <span className={style.candidateData}>{formatedBirthday}</span>
+            </div>
+            <div className={style.cardInfo}>
+              <p className={style.candidateInfo}>Education:</p>
+              <div className={style.candidateData}>
+                {selectedCandidate.education}
+              </div>
+            </div>
+          </div>
         </div>
-
       </div>
-    </div>
-    <Table/>
+      <Table />
     </Fragment>
   );
 }
--- src/pages/Table.jsx
+++ src/pages/Table.jsx
@@ -1,38 +1,55 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { reports } from "../data/reports";
+import style from "./Table.module.css";
+import { EyeFill } from "react-bootstrap-icons";
 
 export const Table = () => {
-    let { id } = useParams()
-    let [data, setData] = useState();
-    let dataTest = []
-    const prepData = () => {
-       for (let i = 0; i<reports.length; i++){
-           if (reports[i].candidateId == id){
-       
-        dataTest.push(reports[i]) 
-        setData(dataTest)
-        console.log(reports[i])
-        console.log(dataTest)
+  let { id } = useParams();
+  let [data, setData] = useState([]);
+  let dataTest = [];
+  const prepData = () => {
+    for (let i = 0; i < reports.length; i++) {
+      if (reports[i].candidateId == id) {
+        dataTest.push(reports[i]);
+        setData(dataTest);
+        console.log(reports[i]);
+        console.log(dataTest);
+      }
     }
-       } 
-    }
-    
-    useEffect(() => {
-        prepData();
-      }, []);
-    console.log(dataTest)
-    
-    return (
-        <table className="container">
-    <tr>
-    <th>Company</th>
-    <th>Interview Date</th>
-    <th>Status</th>
-  </tr>
-  
- 
-  {console.log(data)}
+  };
+
+  useEffect(() => {
+    prepData();
+  }, []);
+  console.log(dataTest);
+
+  return (
+    <table className={`${style.table} container`}>
+      <thead className={style.th}>
+        <tr>
+          <th>Company</th>
+          <th>Interview Date</th>
+          <th colSpan="2">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((d) => (
+          <tr className={style.tr}>
+            <td>{d.companyName}</td>
+            <td>{d.interviewDate}</td>
+            <td
+              className={d.status === "passed" ? style.passed : style.declined}
+            >
+              {d.status}
+            </td>
+            <td>
+              <EyeFill />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      {console.log(data)}
     </table>
-    )
-}
+  );
+};
