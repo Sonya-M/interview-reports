2ebc79e978268839c42e4266d7a23206c3966fa3
Dragana <dragana.bogicevic83@gmail.com>
2021-09-10 15:19:01 +0200

    commit to save changes and swich to main

--- src/components/CandidateList.jsx
+++ src/components/CandidateList.jsx
@@ -3,24 +3,26 @@ import { candidates } from "../data/candidates";
 
 import CandidateCard from "./UI/CandidateCard.jsx"
 
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 
 export default function CandidateList(props) {
   return (
     <Fragment>
+      <Form as={Row} className="m-5">
+        <Col sm={4} className="offset-sm-4" >
+          <Form.Control type="text" size="sm" />
+        </Col>
+      </Form>
       <Row  className="g-4">
-      <Col xs={6} md={4}>
         {candidates.map( c => (
-      
+          <Col xs={6} md={4}>
             <CandidateCard 
               key={c.id}
               candidate={c}
              /> 
-          
+           </Col>   
         ))}
-  </Col>
-</Row>
-      
+      </Row>
     </Fragment>
   );
 }
