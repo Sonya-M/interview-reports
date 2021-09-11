fd432c0541e53352176ea95b7024b436cb87758f
Dragana <dragana.bogicevic83@gmail.com>
2021-09-11 15:13:56 +0200

    commiting changes because of pulling request on main

---
+++ src/components/SingleReport.jsx
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+
+import { Modal, Row, Col } from "react-bootstrap";
+import { GrClose } from "react-bootstrap-icons";
+
+const SingleReport = (props) => {
+  const [show, setShow] = useState(false);
+  
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
+    return (
+      <Fragment>
+        <Modal as={Row} show={show} onHide={handleClose}>
+            <Modal.Header as={Col} closeButton>
+              <Modal.Title>Zula Feeney</Modal.Title>
+            </Modal.Header>
+            <Button as={Col} variant="secondary" onClick={handleClose}>
+              <GrClose size="1rem"/>
+            </Button>
+          <Modal.Body as={Row}>
+              <Col md={3} sm={12}>
+                <h6>Company</h6>
+                <p>Google</p>
+                <h6>Interview Date</h6>
+                <p>20.12.2017</p>
+                <h6>Phase</h6>
+                <p>Tech</p>
+                <h6>Status</h6>
+                <p>passed</p>
+              </Col>
+              <Col md={9} sm={12}>
+                <h6>Notes</h6>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aperiam tempora nostrum accusamus quas dignissimos consequatur velit perspiciatis iure magni molestiae quasi, adipisci veritatis doloremque! Impedit omnis accusantium sed ad!</p>
+              </Col>
+          </Modal.Body>
+        </Modal>
+      </Fragment>
+    );
+
+
+}
+
+export default SingleReport;
