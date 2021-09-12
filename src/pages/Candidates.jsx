78701ceb7d05901922e60112ecdfec3de892b6f3
Sonya-M <sonja.musicki@gmail.com>
2021-09-12 20:27:10 +0200

    lifted state up for improved search + added a clear input btn

--- src/pages/Candidates.jsx
+++ src/pages/Candidates.jsx
@@ -3,24 +3,31 @@ import React, { Fragment, useState, useEffect } from "react";
 import CandidateCommunicator from "../services/CandidateCommunicator";
 
 import CandidateList from "../components/CandidateList";
+import SearchBar from "../components/SearchBar";
 
 const Candidates = (props) => {
   const [candidates, setCandidates] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     CandidateCommunicator.getAll().then((data) => {
       console.log("Fetched candidates", data);
       setCandidates(data);
     });
-
   }, []);
-  if(candidates.length === 0) {
-    return <div>Loading ...</div>
+
+  const handleSearch = (filterText) => {
+    setSearchText(filterText);
+  };
+
+  if (candidates.length === 0) {
+    return <div>Loading ...</div>;
   }
 
   return props.loggedIn ? (
     <Fragment>
-      <CandidateList candidates={candidates} />
+      <SearchBar onSearch={handleSearch} />
+      <CandidateList candidates={candidates} searchText={searchText} />
     </Fragment>
   ) : (
     <Fragment />
