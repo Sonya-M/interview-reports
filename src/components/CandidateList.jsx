ea2bc13a02433f86efa38534d2dc9e043a071134
Sonya-M <sonja.musicki@gmail.com>
2021-09-12 15:20:03 +0200

    added 'No results' message to search, now really

--- src/components/CandidateList.jsx
+++ src/components/CandidateList.jsx
@@ -1,45 +1,45 @@
 import React, { Fragment, useState, useEffect } from "react";
 
-
-import CandidateCard from "./CandidateCard.jsx"
+import CandidateCard from "./CandidateCard.jsx";
 import SearchBar from "./SearchBar.jsx";
-import { includesIgnoreCase } from "../utilities/helpers.js"
+import { includesIgnoreCase } from "../utilities/helpers.js";
 
 import { Row } from "react-bootstrap";
+import ErrorDisplay from "../pages/ErrorDisplay.jsx";
 
-
-
-const CandidateList = ({candidates}) => {
+const CandidateList = ({ candidates }) => {
   const [searchResult, setSearchResult] = useState([]);
 
   useEffect(() => {
     setSearchResult(candidates);
-  },[])
+  }, []);
 
-  
   const filterCandidates = (event) => {
     let filtered = [];
-    if(event.target.value) {
-      filtered = candidates.filter(candidate => includesIgnoreCase(candidate.name, event.target.value));
+    if (event.target.value) {
+      filtered = candidates.filter((candidate) =>
+        includesIgnoreCase(candidate.name, event.target.value)
+      );
       setSearchResult(filtered);
     } else {
       setSearchResult(candidates);
     }
-  }
+  };
 
-  return(
+  return (
     <Fragment>
-      <SearchBar filterCandidates={filterCandidates}/>
-      <Row  className="g-4 m-5">
-        {searchResult.map( item => (
-          <CandidateCard 
-            key={item.id}
-            candidate={item}
-            /> 
-        ))}
-      </Row>
+      <SearchBar filterCandidates={filterCandidates} />
+      {searchResult.length > 0 ? (
+        <Row className="g-4 m-5">
+          {searchResult.map((item) => (
+            <CandidateCard key={item.id} candidate={item} />
+          ))}
+        </Row>
+      ) : (
+        <ErrorDisplay message="No results" />
+      )}
     </Fragment>
-  )
-} 
+  );
+};
 
 export default CandidateList;
