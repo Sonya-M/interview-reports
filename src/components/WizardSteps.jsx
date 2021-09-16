fdcc0e618c545bd65ef3675051c8ac58cc488f50
Sonya-M <sonja.musicki@gmail.com>
2021-09-16 13:06:43 +0200

    first commit

---
+++ src/components/WizardSteps.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import styles from "./WizardSteps.module.css";
+
+export default function WizardSteps(props) {
+  const stepsDesc = [
+    "Select Candidate",
+    "Select Company",
+    "Fill Report Details",
+  ];
+
+  const steps = [];
+  for (let i = 0; i < 3; i++) {
+    let classes = i === props.currentStep ? styles.currentStep : "";
+    steps.push(
+      <li className={classes} key={i}>
+        {stepsDesc[i]}
+      </li>
+    );
+  }
+  return <ol className={styles.stepList}>{steps}</ol>;
+}
