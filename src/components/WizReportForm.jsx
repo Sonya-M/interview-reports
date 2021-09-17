1132b6f3b2a2c5e2e68ddfeec35798f8a0d74af0
Sonya-M <sonja.musicki@gmail.com>
2021-09-17 14:34:05 +0200

    added form validation with useReducer

--- src/components/WizReportForm.jsx
+++ src/components/WizReportForm.jsx
@@ -1,42 +1,114 @@
-import React, { useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import { formatDateForHtmlInput } from "../utilities/helpers";
 import { Row, Col, Form, FormGroup, Button } from "react-bootstrap";
+import styles from "./WizReportForm.module.css";
+
+const inputReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val !== "" };
+  }
+  if (action.type === "INPUT_BLUR") {
+    // `state`: latest state snapshot, guaranteed to be up to date
+    return { value: state.value, isValid: state.value !== "" };
+  }
+  // default:
+  return { value: "", isValid: false };
+};
 
 export default function WizReportForm(props) {
-  const [formInput, setFormInput] = useState({});
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [markInvalid, setMarkInvalid] = useState(false); // mark invalid input
+  //  only after first attempt to submit
+  const [dateState, dispatchDate] = useReducer(inputReducer, {
+    value: "",
+    isValid: null, // !!! with null as init val, not treated as invalid
+  });
+  const [phaseState, dispatchPhase] = useReducer(inputReducer, {
+    value: "",
+    isValid: null,
+  });
+  const [statusState, dispatchStatus] = useReducer(inputReducer, {
+    value: "",
+    isValid: null,
+  });
+  const [noteState, dispatchNote] = useReducer(inputReducer, {
+    value: "",
+    isValid: null,
+  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    console.log("name", name, "value:", value);
-
-    setFormInput((prevInput) => ({ ...prevInput, [name]: value }));
+  const handleDateChange = (e) => {
+    dispatchDate({ type: "USER_INPUT", val: e.target.value });
+  };
+  const handlePhaseChange = (e) => {
+    dispatchPhase({ type: "USER_INPUT", val: e.target.value });
+  };
+  const handleStatusChange = (e) => {
+    dispatchStatus({ type: "USER_INPUT", val: e.target.value });
+  };
+  const handleNoteChange = (e) => {
+    dispatchNote({ type: "USER_INPUT", val: e.target.value });
   };
 
+  useEffect(() => {
+    const timerID = setTimeout(() => {
+      setFormIsValid(
+        dateState.isValid,
+        phaseState.isValid,
+        statusState.isValid,
+        noteState.isValid
+      );
+    }, 300);
+
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, [
+    dateState.isValid,
+    phaseState.isValid,
+    statusState.isValid,
+    noteState.isValid,
+  ]); // within useEffect, can set state based on other states because those other
+  // states in the dependency array are guaranteed to be up to date
+
   const handleSubmit = (e) => {
-    console.log("handle submit: input: ", formInput);
     e.preventDefault();
-    props.onSubmit(formInput);
+    if (!formIsValid) {
+      setMarkInvalid(true);
+      return;
+    }
+    props.onSubmit({
+      interviewDate: dateState.value,
+      phase: phaseState.value,
+      status: statusState.value,
+      note: noteState.value,
+    });
   };
 
   const maxDate = formatDateForHtmlInput(new Date());
-  console.log("Returned date format: ", maxDate);
+  // console.log("Returned date format: ", maxDate);
+
   return (
     <FormGroup className="m-3">
-      <Row className="g-2">
+      <Row className="g-3">
         <Col lg>
           <Form.Control
+            className={markInvalid && !dateState.isValid ? styles.invalid : " "}
             name="interviewDate"
             type="date"
             max={maxDate}
-            value={formInput.date}
-            onChange={handleChange}
+            value={dateState.value}
+            onChange={handleDateChange}
+            // style={markInvalid && !dateState.isValid ? invalidStyle : null}
           />
         </Col>
         <Col md>
           <Form.Select
+            className={
+              markInvalid && !phaseState.isValid ? styles.invalid : " "
+            }
             name="phase"
-            value={formInput.phase}
-            onChange={handleChange}
+            value={phaseState.value}
+            onChange={handlePhaseChange}
           >
             <option value="">Select phase</option>
             <option value="CV">CV</option>
@@ -47,9 +119,12 @@ export default function WizReportForm(props) {
         </Col>
         <Col md>
           <Form.Select
+            className={
+              markInvalid && !statusState.isValid ? styles.invalid : " "
+            }
             name="status"
-            value={formInput.status}
-            onChange={handleChange}
+            value={statusState.value}
+            onChange={handleStatusChange}
           >
             <option value="">Select status</option>
             <option value="Passed">Passed</option>
@@ -60,12 +135,20 @@ export default function WizReportForm(props) {
         <Col sm={12}>
           <Form.Control
             as="textarea"
+            className={markInvalid && !noteState.isValid ? styles.invalid : " "}
             placeholder="Notes"
             name="note"
-            value={formInput.note}
-            onChange={handleChange}
+            value={noteState.value}
+            onChange={handleNoteChange}
             style={{ height: "150px" }}
           />
+        </Col>
+        <Col sm={12}>
+          {markInvalid && !formIsValid ? (
+            <p className={styles.invalidMsg}>Please fill out all form fields</p>
+          ) : (
+            <React.Fragment />
+          )}
         </Col>
       </Row>
       <div className="d-flex justify-content-between">
