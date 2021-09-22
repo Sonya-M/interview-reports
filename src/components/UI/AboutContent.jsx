3d1cd3982a9877d63a9a6f5eb5921f83d20cf735
Sonya-M <sonja.musicki@gmail.com>
2021-09-22 10:09:31 +0200

    replaced list item images with icons and unicode symbols

--- src/components/UI/AboutContent.jsx
+++ src/components/UI/AboutContent.jsx
@@ -9,21 +9,57 @@ const AboutContent = () => {
       <div className={style.brandBackground}>
         <div className={style.brandName}>BigBangTeam</div>
       </div>
-      <div className={style.textContent}><p>We are a motley crew of geeks who joined forces on their final project at BiT Bootcamp. JobBook is a React app for viewing and creating reports about candidates and their job applications in various companies. The technologies used for the project include:</p>
-      <ul className={style.tehnology}>
-          <li className={style.html5}>HTML5</li>
-          <li className={style.css3}>CSS3</li>
-          <li className={style.boots}>BootStrap</li>
-          <li className={style.react}>React (JavaScript)</li>
-      </ul>
-      <p>A huge thank you to our instructors:</p>
-      <ul className={style.professors}>
-          <li><a href="https://www.linkedin.com/in/ivanbalic2/" target="_blank" >Ivan Balic</a></li>
-          <li><a href="https://www.linkedin.com/in/nikola-ali%C4%87-0310b0138/" target="_blank" >Nikola Alic</a></li>
-          <li><a href="https://www.linkedin.com/in/tijana-pavlovic-304a27bb/" target="_blank" >Tijana Pavlovic</a></li>
-      </ul>
-       For anything else you are curious about, feel free to contact us.</div>
-      <Developers/>
+      <div className={style.textContent}>
+        <p>
+          We are a motley crew of geeks who joined forces on their final project
+          at the BiT Bootcamp. JobBook is a React app for viewing and creating
+          reports about candidates and their job applications in various
+          companies. The technologies used for the project include:
+        </p>
+        <ul className={style.tehnology}>
+          <li>
+            <i class="devicon-html5-plain"></i> HTML5
+          </li>
+          <li>
+            <i class="devicon-css3-plain"></i> CSS3
+          </li>
+          <li>
+            <i class="devicon-bootstrap-plain"></i> BootStrap
+          </li>
+          <li>
+            <i class="devicon-react-original"></i> React (JavaScript)
+          </li>
+        </ul>
+        <p>A huge thank you to our instructors:</p>
+        <ul className={style.professors}>
+          <li>
+            <a href="https://www.linkedin.com/in/ivanbalic2/" target="_blank">
+              {" "}
+              Ivan Balic
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/nikola-ali%C4%87-0310b0138/"
+              target="_blank"
+            >
+              {" "}
+              Nikola Alic
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/tijana-pavlovic-304a27bb/"
+              target="_blank"
+            >
+              {" "}
+              Tijana Pavlovic
+            </a>
+          </li>
+        </ul>
+        For anything else you are curious about, feel free to contact us.
+      </div>
+      <Developers />
     </RisingContainer>
   );
 };
