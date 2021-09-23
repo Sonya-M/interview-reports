21647a9b4bd83ad094baaeda8124762081efd260
Sonya-M <sonja.musicki@gmail.com>
2021-09-23 19:45:16 +0200

    fixed warnings

--- src/components/CandidateCard.jsx
+++ src/components/CandidateCard.jsx
@@ -4,43 +4,36 @@ import { Link } from "react-router-dom";
 import ImageGuaranteed from "./UI/ImageGuaranteed.jsx";
 import { PLACEHOLDER_IMG } from "../shared/constants";
 
-import { Card, Col } from "react-bootstrap";
 import style from "./CandidateCard.module.css";
-import AdminPage, {candidateName, candidateId} from "../pages/AdminPage" 
-
 
 const CandidateCard = (props) => {
   const { candidate } = props;
-  
- 
-    
+
   return (
     <Fragment>
-      
-        <Link to={"/candidates/" + candidate.id} className={style.linksStyle}>
-          <div className={`${style.candidateCard} `}>
-            {candidate.avatar ? (
-              <ImageGuaranteed
-                preferredImg={candidate.avatar}
-                placeholderImg={PLACEHOLDER_IMG}
-                preferredImgAlt={candidate.name}
-                alt="avatar"
-                className={style.avatar}
-              />
-            ) : (
-              <img alt="No file available" src={PLACEHOLDER_IMG} className={style.avatar} />
-            )}
-            <div className={style.candidateInfo}>
-              <div className={style.name}>
-                {candidate.name}
-              </div>
-              <div className={style.email}>
-                {candidate.email}
-              </div>
-            </div>
+      <Link to={"/candidates/" + candidate.id} className={style.linksStyle}>
+        <div className={`${style.candidateCard} `}>
+          {candidate.avatar ? (
+            <ImageGuaranteed
+              preferredImg={candidate.avatar}
+              placeholderImg={PLACEHOLDER_IMG}
+              preferredImgAlt={candidate.name}
+              alt="avatar"
+              className={style.avatar}
+            />
+          ) : (
+            <img
+              alt="No file available"
+              src={PLACEHOLDER_IMG}
+              className={style.avatar}
+            />
+          )}
+          <div className={style.candidateInfo}>
+            <div className={style.name}>{candidate.name}</div>
+            <div className={style.email}>{candidate.email}</div>
           </div>
-        </Link>
-      
+        </div>
+      </Link>
     </Fragment>
   );
 };
